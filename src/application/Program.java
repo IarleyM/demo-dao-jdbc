5d97ce2b12d68d795781e3f6d0069c3e16453f58
Iarley Melo <iarleym771@gmail.com>
2023-01-31 16:08:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department dep = new Department(1, "books");
 		System.out.println(dep);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com",new Date(), 3000.00, dep);
+		System.out.println(seller);
 	}
 
 }
