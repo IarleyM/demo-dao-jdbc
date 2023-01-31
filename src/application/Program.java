957bac277543116c673019a9f6878cbe3fcba8cc
Iarley Melo <iarleym771@gmail.com>
2023-01-31 16:40:02 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "books");
-		System.out.println(dep);
-		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com",new Date(), 3000.00, dep);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
